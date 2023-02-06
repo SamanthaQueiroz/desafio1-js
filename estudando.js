0c6509a329d7dc57285c961d871e2398fe6ac605
SamanthaQueiroz <ssequeiroz@gmail.com>
2023-02-06 15:58:18 -0300

    subindo arquivo de estudo sobre js

---
+++ estudando.js
@@ -0,0 +1,10 @@
+
+    let quantidadeDeMulheresNaAula = prompt("quantidade de Mulheres:")
+    let quantidadeDeHomensNaAula = prompt("quantidade de Homens:")
+
+    let alunas = parseInt(quantidadeDeMulheresNaAula)
+    let alunos = parseInt(quantidadeDeHomensNaAula)
+
+    let totalDeAlunosNaAula = alunas + alunos
+
+    console.log (totalDeAlunosNaAula)
